57c687d3ad62292b52efec9959079dccd9852dbe
curto19 <curt.whitmire@noaa.gov>
2019-04-30 22:35:05 -0700

    Updated tmap plot script, 3 plots

--- SurveyExtents_plot.R
+++ SurveyExtents_plot.R
@@ -6,31 +6,33 @@ library(tmap)
 
 # Set working directory
 current_path <- getActiveDocumentContext()$path
-# setwd("/Users/curt.whitmire/Documents/GIS/AnalysisProgram/Assessment/Assessment2019/layers")
 setwd(dirname(current_path))
 print(getwd())
-
-# # Getting rid of anything saved in your workspace 
-rm(list=ls())
 
 # import shapefile
 extents <- readOGR("SurveyExtent_diss_merge.shp")
 coast <- readOGR("WCstates_BC_Mex_100K.shp")
 EEZ <- readOGR("pacific_eez_segmented.shp")
 
-# summary(extents)
-extents <- extents[extents@data$Survey == "NWFSC Slope",]
+# filter on survey
+summary(extents)
+extents <- extents[extents@data$Survey == "AFSC Triennial",]
 extents <- extents[extents@data$Year != "1999",]
 extents <- extents[extents@data$Year != "2000",]
 extents <- extents[extents@data$Year != "2001",]
+extents <- extents[extents@data$Survey != "AFSC Slope",]
+extents <- extents[extents@data$Survey != "NWFSC Slope",]
+extents <- extents[extents@data$Survey != "NWFSC Shelf-Slope",]
 # summary(extents)
 
 # Open a pdf file
+pdf(file = "AFSC_Slope_AllYrs.pdf", width = 10, height = 10)
+pdf(file = "Triennial_AllYrs.pdf", width = 10, height = 10)
 pdf(file = "NWFSC_Slope_AllYrs.pdf", width = 10, height = 6)
 # plot
 current.mode <- tmap_mode("plot")
 tm_shape(extents) + tm_fill("Year", thres.poly = 0) + 
-  tm_facets(by = "SurveyYear", nrow = 1, free.coords = FALSE) +
+  tm_facets(by = "SurveyYear", nrow = 2, free.coords = FALSE) +
   tm_shape(coast) + tm_polygons() +
   tm_shape(EEZ) + tm_lines() +
   tm_layout(legend.show = FALSE, 
