25b479ee7389f0ab78d234b2b571405d2fc6b193
Curt-Whitmre-NOAA <curt.whitmire@noaa.gov>
2019-05-14 14:22:50 -0700

    Updated plot script to include two plots options: one with survey extents only, and another adding PacFIN data for positive hauls of sablefish

--- SurveyExtents_plot.R
+++ SurveyExtents_plot.R
@@ -14,11 +14,20 @@ print(getwd())
 extents <- readOGR("SurveyExtent_diss_merge.shp")
 coast <- readOGR("WCstates_BC_Mex_100K.shp")
 EEZ <- readOGR("pacific_eez_segmented.shp")
-sabPts <- st_as_sf(PacFIN.Logbook.Sab.Tows, coords = c("Best_Long","Best_Lat"))
+
 # Add field for faceting on geographic subregion (WA, OR, NorCal, CenCal, SoCal)
-sabPts %>% 
-  filter(Best_Lat >= 46 & Best_Lat < 50) %>% 
-  mutate(region = "Washington")
+sabTows <- PacFIN.Logbook.Sab.Tows %>%
+  mutate(
+    region = case_when(
+    Best_Lat > 46 ~ "1-Washington",
+    Best_Lat <= 46 & Best_Lat > 42 ~ "2-Oregon",
+    Best_Lat <= 42 & Best_Lat > 38 ~ "3-NorCal",
+    Best_Lat <= 38 & Best_Lat > 34 ~ "4-CenCal",
+    Best_Lat <= 34 & Best_Lat > 30 ~ "5-SoCal"
+  ))
+
+# Convert PacFIN hauls to shape object
+sabPts <- st_as_sf(sabTows, coords = c("Best_Long","Best_Lat"), crs = "+proj=longlat +datum=WGS84")
 
 # filter on survey
 summary(extents)
@@ -29,7 +38,6 @@ extents <- extents[extents@data$Year != "2001",]
 extents <- extents[extents@data$Survey != "AFSC Slope",]
 extents <- extents[extents@data$Survey != "NWFSC Slope",]
 extents <- extents[extents@data$Survey == "NWFSC Shelf-Slope",]
-# summary(extents)
 
 # Open a pdf file
 pdf(file = "AFSC_Slope_AllYrs.pdf", width = 10, height = 10)
@@ -38,21 +46,28 @@ pdf(file = "NWFSC_Slope_AllYrs.pdf", width = 10, height = 6)
 # plot
 current.mode <- tmap_mode("plot")
 tm_shape(extents) + tm_fill("Year", thres.poly = 0) + 
-  # tm_facets(by = "SurveyYear", nrow = 2, free.coords = FALSE) +
-  tm_shape(sabPts) + tm_dots(col = "RYEAR") +
+  tm_facets(by = "SurveyYear", nrow = 2, free.coords = FALSE) +
   tm_shape(coast) + tm_polygons() +
   tm_shape(EEZ) + tm_lines() +
   tm_layout(legend.show = FALSE, 
             title.position = c("center", "center"), title.size = 20, 
             panel.label.bg.color = "white", panel.label.fontface = "bold"
             )
+# Close the pdf file
+dev.off()
 
-# Found this code in nwfscMapping package. Doesn't work yet.
-library(nwfscMapping)
-multiMap(nrows=1,ncols=2,lonLims=matrix(c(-125,-125,-122,-123),nrow=2),latLims=matrix(c(42,45,45,46),nrow=2),fnc=plotMap,polys=extents)
-multiMap(nrows=1,ncols=3,
-         lonLims=matrix(c(-123.1,-122.9,-125,-121,-125,-121),nrow=3,byrow=T),
-         latLims=matrix(c(37,40,40,45,45,48),nrow=3,byrow=T),
-         fnc=plotMap,polys=extents)
+# Open a pdf file
+pdf(file = "NWFSC_Comb_wSabTows.pdf", width = 10, height = 8)
+# plot with PacFIN hauls as dots
+current.mode <- tmap_mode("plot")
+tm_shape(extents) + tm_fill() + 
+  tm_shape(sabPts) + tm_dots(col = "RYEAR") +
+  tm_facets(by = "region", nrow = 2, free.coords = TRUE) +
+  tm_shape(coast) + tm_polygons() + tm_text("STATE_NAME", auto.placement = TRUE) +
+  tm_shape(EEZ) + tm_lines() +
+  tm_layout(legend.show = FALSE, 
+            title.position = c("center", "center"), title.size = 20, 
+            panel.label.bg.color = "white", panel.label.fontface = "bold"
+  )
 # Close the pdf file
 dev.off()
