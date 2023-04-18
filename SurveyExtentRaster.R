a64e584b1e3fbe6eea176d2664f9bd8c735c3865
Curt-Whitmire-NOAA <curt.whitmire@noaa.gov>
2023-04-18 15:47:16 -0700

    Cleaned up script to output raster in both Albers and UTM projections

--- SurveyExtentRaster.R
+++ SurveyExtentRaster.R
@@ -1,53 +1,55 @@
 # Script to generate bland raster for U.S. West Coast Continental Margin
 # and clip to NMFS West Coast Groundfish Bottom Trawl Survey extent
 # Workflow adopted from https://rspatial.org/raster/pkg/3-objects.html, and
-# https://geobgu.xyz/r-2020/combining-rasters-and-vector-layers.html
+# https://geobgu.xyz/r-2020/combining-rasters-and-vector-layers.html, and
+# https://datacarpentry.org/semester-biology/materials/spatial-data-cropping-R/
 
+# Load libraries
 library(sf)
 library(raster)
 library(stars)
-# library(gridExtra)
 
-# load shapefiles
-# gridBTSdd <- st_read("/Users/curt.whitmire/Documents/github/_data/GIS/WCGBTS_Grid_v2008_dd/WCGBTS_Grid_v2008_dd.shp")
-# st_crs(gridBTSdd)
-gridBTSutm <- st_read("/Users/curt.whitmire/Documents/github/_data/GIS/WCGBTS_Grid_v2008_utm/WCGBTS_Grid_v2008_utm.shp")
+# Import shapefiles
+gridBTSutm <- sf::st_read("/Users/curt.whitmire/Documents/github/_data/GIS/WCGBTS_Grid_v2008_utm/WCGBTS_Grid_v2008_utm.shp")
+# Rview crs of imported shapefile
 st_crs(gridBTSutm)
-# gridBTSalb <- st_read("/Users/curt.whitmire/Documents/github/_data/GIS/WCGBTS_Grid_v2008_alb/WCGBTS_Grid_v2008_alb.shp")
-# st_crs(gridBTSalb)
+gridBTSalb <- sf::st_read("/Users/curt.whitmire/Documents/github/_data/GIS/WCGBTS_Grid_v2008_alb/WCGBTS_Grid_v2008_alb.shp")
+# Review crs of imported shapefile
+st_crs(gridBTSalb)
 
-# Create RasterLayer with the extent parameters from shapefile
-# x <- raster(ncol=817, nrow=1822, xmn=st_bbox(gridBTSalb)[1], xmx=st_bbox(gridBTSalb)[3], ymn=st_bbox(gridBTSalb)[2], ymx=st_bbox(gridBTSalb)[4], vals=1)
-x <- raster(ncol=761, nrow=1829, xmn=st_bbox(gridBTSutm)[1], xmx=st_bbox(gridBTSutm)[3], ymn=st_bbox(gridBTSutm)[2], ymx=st_bbox(gridBTSutm)[4], vals=1)
-# x <- raster(ncol=105, nrow=198, xmn=st_bbox(gridBTSdd)[1], xmx=st_bbox(gridBTSdd)[3], ymn=st_bbox(gridBTSdd)[2], ymx=st_bbox(gridBTSdd)[4], vals=1)
+# Define the raster variables, including crs, resolution, abd default cell values
+projUTM <- "+proj=utm +zone=10 +datum=WGS84" # WGS84 UTM Zone 10
+projALB <- "+proj=aea +lat_1=32 +lat_2=48 +lat_0=40 +lon_0=-126 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs" # Custom NAD83 Albers
+hres <- 1000
+rval <- 1
 
-# set the coordinate reference system (CRS) (define the projection)
-projection(x) <- "+proj=utm +zone=10 +datum=WGS84"
-
-# Describe RasterLayer
+# Create RasterLayer object with the extent parameters from shapefile
+x <- raster::raster(resolution=hres, # set horizontal resolution
+                    xmn=st_bbox(gridBTSalb)[1], 
+                    xmx=st_bbox(gridBTSalb)[3], 
+                    ymn=st_bbox(gridBTSalb)[2], 
+                    ymx=st_bbox(gridBTSalb)[4], 
+                    crs=projALB, # set crs
+                    vals=rval) # set default cell value
+# Describe RasterLayer object
 x
-res(x)
-
-# change resolution
-res(x) <- 1000
-res(x)
-ncol(x)
-nrow(x)
 
 # Convert raster to stars object
-r <- st_as_stars(x)
-r
-# Crop the raster by the shapefile
-r_crop <- r[gridBTSutm, crop=TRUE]
-# r_crop <- st_crop(r, gridBTSutm)
-dim(r)
-dim(r_crop)
+r_alb <- stars::st_as_stars(x)
+r_alb
 
-# Plot the rasters, side by side
-par(mfrow = c(1, 2))
-raster::plot(r)
-raster::plot(r_crop)
-par(mfrow = c(1,1))
+# Crop the raster by the extent shapefile
+r_alb <- r_alb[gridBTSalb]
+r_alb
 
-# Save output
-write_stars(r_crop, "WCGBTS_raster.tif")
+# Project cropped raster to UTM
+r_utm = st_warp(r_alb, crs = st_crs(gridBTSutm))
+r_utm
+
+# Plot the cropped raster
+plot(r_alb, reset = FALSE)
+plot(st_geometry(gridBTSalb), add = TRUE)
+
+# Save output rasters
+stars::write_stars(r_alb, paste0("WCGBTS_raster_", hres, "m_alb.tif"))
+stars::write_stars(r_utm, paste0("WCGBTS_raster_", hres, "m_utm.tif"))
